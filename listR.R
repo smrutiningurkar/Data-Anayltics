0578b16d7ac2424d987b8fa348c73a5967900764
smrutiningurkar <70842435+smrutiningurkar@users.noreply.github.com>
2020-09-26 15:36:06 +0530

    Add files via upload

---
+++ listR.R
@@ -0,0 +1,29 @@
+vect  <- 1:5
+
+#Creating a 2x 5 matrix
+
+mat  <- matrix(1:10, ncol = 5)
+
+dim(mat)
+
+# Creating a data frame by selecting the 10th row of the built-in R data set EuStockMarkets
+
+df <- EuStockMarkets[1:10,]
+
+# Construct list with these vec, mat, and df:
+
+my_list <- list(vect, mat, df)
+
+my_list
+
+
+
+#Select Elements from List
+
+#We need to use the [[index]] to select an element in a list. 
+
+#The value inside the double square bracket represents the position of the item in a list we want to extract.
+
+# Print second element of the list
+
+my_list[[2]]
